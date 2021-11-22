16bd2d14321792bd9af1c6c2725fe5cf679544fe
Afolayan Stephen <afolayan4life@gmail.com>
2021-11-22 09:15:25 +0100

    Designed the form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
+import bigBanner from './images/banner-big.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"container"}>
+      <img src={logo} className="logo"/>
+      <div className={"card"}>
+        <div className={"card-image card-size"}></div>
+        <div className={"card-content card-size"}>
+          <form className="form">
+            <div className="form-row">
+              <div className="form-column">
+                <input type="text" name="firstname" placeholder="Firstname" className="form-input" />
+              </div>
+              <div className="form-column">
+                <input type="text" name="lastname" placeholder="Lastname" className="form-input" />
+              </div>
+            </div>
+            <div className="form-group">
+              <input type="phone" name="phone" placeholder="Phone Number" className="form-input" />
+            </div>
+            <div className="form-group">
+              <input type="email" name="email" placeholder="Email Address" className="form-input" />
+            </div>
+            <div className="form-group">
+              <select className="form-input">
+                <option value="">How did you hear about us?</option>
+                <option value="facebook">Facebook</option>
+                <option value="google">Google</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <input type="submit" value="Register" className="form-button" />
+            </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
