943fc0b4aaf329564133ee965c3d52e17ad72574
Afolayan Stephen <afolayan4life@gmail.com>
2021-12-01 10:49:24 +0100

    Fixed form error state bugs

--- src/App.js
+++ src/App.js
@@ -17,16 +17,21 @@ function App() {
   const [emailAddress, setEmailAddress] = useState("");
   const [referral, setReferral] = useState("");
 
-  const [hasError, setHasError] = useState({
-    firstname: false,
-    lastname: false,
-    phone_number: false,
-    email_address: false,
-    referral: false,
-  });
-  const[errorContent, setErrorContent] = useState("");
+  const [firstnameError, setFirstnameError] = useState(false);
+  const [lastnameError, setLastnameError] = useState(false);
+  const [phoneNumberError, setPhoneNumberError] = useState(false);
+  const [emailAddressError, setEmailAddressError] = useState(false);
+  const [referralError, setReferralError] = useState(false);
 
+  // const [hasError, setHasError] = useState({
+  //   firstname: false,
+  //   lastname: false,
+  //   phone_number: false,
+  //   email_address: false,
+  //   referral: false,
+  // });
 
+  const[formSuccess, setFormSuccess] = useState(false);
 
   const handleFirstNameChanged = (e) => {
     setFirstname(e.target.value);
@@ -58,13 +63,27 @@ function App() {
       email_address: emailAddress,
       referral: referral
     }).then(function(response){
-      console.log(response);
+      setFormSuccess(true);
     }).catch(function(error){
       console.log(error);
-      // alert(error.response.data.messages.firstname);
-      if(error.response.data.messages.hasOwnProperty("firstname")){
-        setHasError({...hasError, firstname: true });
-        console.log(hasError.firstname);
+      if('firstname' in error.response.data.messages){
+        setFirstnameError(true);
+      }
+
+      if('lastname' in error.response.data.messages){
+        setLastnameError(true); 
+      }
+
+      if('phone_number' in error.response.data.messages){
+        setPhoneNumberError(true); 
+      }
+
+      if('email_address' in error.response.data.messages){
+        setEmailAddressError(true);
+      }
+
+      if('referral' in error.response.data.messages){
+        setReferralError(true);
       }
     });
   }
@@ -79,8 +98,8 @@ function App() {
         <div className={"card-image card-size"}></div>
         <div className={"card-image-mobile"}></div>
         <div className={"card-content card-size"}>
-            <div className="info-card">
-              <div className="info-card-contents info-card-error">
+            <div className={ formSuccess === true ? "info-card" : "hide"}>
+              <div className="info-card-contents info-card-success">
                 <p>Registration Successful</p>
               </div>
             </div>
@@ -88,20 +107,20 @@ function App() {
             
             <div className="form-row">
               <div className="form-column">
-                <input type="text" name="firstname" placeholder="Firstname" value={firstname} className="form-input" onChange={ (e) => handleFirstNameChanged(e) }/>
+                <input type="text" name="firstname" placeholder="Firstname" value={firstname} className={ firstnameError ? 'form-input form-input-error': 'form-input' } onChange={ (e) => handleFirstNameChanged(e) }/>
               </div>
               <div className="form-column">
-                <input type="text" name="lastname" placeholder="Lastname" value={lastname} className="form-input" onChange={ (e) => handleLastnameChanged(e) }/>
+                <input type="text" name="lastname" placeholder="Lastname" value={lastname} className={ lastnameError ? 'form-input form-input-error': 'form-input' } onChange={ (e) => handleLastnameChanged(e) }/>
               </div>
             </div>
             <div className="form-group">
-              <input type="phone" name="phone" placeholder="Phone Number" value={phoneNumber} className="form-input" onChange={ (e) => handlePhoneNumberChanged(e) }/>
+              <input type="phone" name="phone" placeholder="Phone Number" value={phoneNumber} className={ phoneNumberError ? 'form-input form-input-error': 'form-input' } onChange={ (e) => handlePhoneNumberChanged(e) }/>
             </div>
             <div className="form-group">
-              <input type="email" name="email" placeholder="Email Address" value={emailAddress} className="form-input" onChange={ (e) => handleEmailAddressChanged(e) }/>
+              <input type="email" name="email" placeholder="Email Address" value={emailAddress} className={ emailAddressError ? 'form-input form-input-error': 'form-input' } onChange={ (e) => handleEmailAddressChanged(e) }/>
             </div>
             <div className="form-group">
-              <select className="form-input" value={referral} onChange={ (e) => handleReferralChanged(e) }>
+              <select className={ referralError ? 'form-input form-input-error': 'form-input' } value={referral} onChange={ (e) => handleReferralChanged(e) }>
                 <option value="">How did you hear about us?</option>
                 <option value="facebook">Facebook</option>
                 <option value="google">Google</option>
